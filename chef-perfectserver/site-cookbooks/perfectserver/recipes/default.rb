055f0260ad14cde580e7d004653ca611e2d71351
Daniel Fanica <contact@danielfanica.com>
2015-12-29 17:24:06 +0000

    web.json

--- chef-perfectserver/site-cookbooks/perfectserver/recipes/default.rb
+++ chef-perfectserver/site-cookbooks/perfectserver/recipes/default.rb
@@ -10,12 +10,12 @@
 chef_gem "chef-rewind"
 require 'chef/rewind'
 
-include_recipe 'ohai'
+# include_recipe 'ohai'
 
 # Basic Packages
-include_recipe "apt"
-include_recipe "ubuntu"
-include_recipe "build-essential"
+# include_recipe "apt"
+# include_recipe "ubuntu"
+# include_recipe "build-essential"
 
 
 # Install packages
@@ -40,8 +40,8 @@ fail2ban
 
 graphicsmagick
 
-libsasl2-2 
-sasl2-bin 
+libsasl2-2
+sasl2-bin
 libsasl2-modules
 
 php5-json
@@ -61,19 +61,19 @@ end
 
 
 # Setup database Server
-include_recipe "mysql"
-include_recipe "mysql::server"
-include_recipe "database::mysql"
+# include_recipe "mysql"
+# include_recipe "mysql::server"
+# include_recipe "database::mysql"
 
 
 
 # Setup apache
-include_recipe "apache2"
-include_recipe "apache2::mod_ssl"
-include_recipe "apache2::mod_php5"
-include_recipe "apache2::mod_rewrite"
-include_recipe "apache2::mod_fastcgi"
-include_recipe "apache2::mod_fcgid"
+# include_recipe "apache2"
+# include_recipe "apache2::mod_ssl"
+# include_recipe "apache2::mod_php5"
+# include_recipe "apache2::mod_rewrite"
+# include_recipe "apache2::mod_fastcgi"
+# include_recipe "apache2::mod_fcgid"
 
 # Additional apache modules not covered by the cookbook
 %w{apache2-suexec}.each do |pkg|
@@ -86,47 +86,47 @@ apache_module "suexec"
 
 
 # Setup php
-include_recipe "php"
-include_recipe "php::module_curl"
-include_recipe "php::module_mysql"
-include_recipe "php::module_gd"
-include_recipe "php::module_apc"
+# include_recipe "php"
+# include_recipe "php::module_curl"
+# include_recipe "php::module_mysql"
+# include_recipe "php::module_gd"
+# include_recipe "php::module_apc"
 
 
 
 # Setup Mail
-include_recipe "postfix"
+# include_recipe "postfix"
 
 rewind "template[/etc/postfix/master.cf]" do
   cookbook "perfectserver"
   source "postfix/master.cf.erb"
 end
 
-include_recipe "postfixgld"
+# include_recipe "postfixgld"
 
 
 
 # Setup pureftpd
-include_recipe "pure-ftpd"
+# include_recipe "pure-ftpd"
 
 
 
 # Tools
-include_recipe "openssl"
+# include_recipe "openssl"
 
 
 
 # Git
-include_recipe "git"
+# include_recipe "git"
 #include_recipe "git::server"
 
 
 
 # Web Applications
-include_recipe "phpmyadmin"
+# include_recipe "phpmyadmin"
 include_recipe "perfectserver::phpmyadmin"
 
-include_recipe "roundcube"
+# include_recipe "roundcube"
 
 
 
