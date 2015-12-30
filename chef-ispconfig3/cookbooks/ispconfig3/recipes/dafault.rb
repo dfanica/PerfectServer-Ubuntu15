1e9632c33c96da637f5645328da839ea02409ee3
Daniel Fanica <contact@danielfanica.com>
2015-12-30 18:20:23 +0000

    revert to dafault

--- chef-ispconfig3/cookbooks/ispconfig3/recipes/dafault.rb
+++ chef-ispconfig3/cookbooks/ispconfig3/recipes/dafault.rb
@@ -11,3 +11,18 @@
 template ::File.join(node['ispconfig3']['sources_list_path'], 'sources.list') do
     source 'sources.list.erb'
 end
+
+# update the system
+# include_recipe "apt"
+execute 'apt-get update'
+execute 'apt-get upgrade' do
+    command 'apt-get upgrade -y'
+    ignore_failure true
+    action :run
+end
+
+# /bin/sh is a symlink to /bin/dash, however we need /bin/bash, not /bin/dash
+link '/bin/sh' do
+  to 'bash'
+  link_type :symbolic
+end
