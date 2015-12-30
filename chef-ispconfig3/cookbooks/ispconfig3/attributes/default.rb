89bef344a56c9707f35ddb1bad96f22b976160c3
Daniel Fanica <contact@danielfanica.com>
2015-12-30 17:59:15 +0000

    sendmail remove with cookbook

--- chef-ispconfig3/cookbooks/ispconfig3/attributes/default.rb
+++ chef-ispconfig3/cookbooks/ispconfig3/attributes/default.rb
@@ -1,2 +1,5 @@
 # ispcongif3
 default['ispconfig3']['sources_list_path'] = '/etc/apt'
+
+# apparmor
+default['apparmor']['disable'] = true
--- chef-ispconfig3/cookbooks/ispconfig3/recipes/prepare.rb
+++ chef-ispconfig3/cookbooks/ispconfig3/recipes/prepare.rb
@@ -21,17 +21,3 @@ link '/bin/sh' do
   to 'bash'
   link_type :symbolic
 end
-
-# bash 'Disable AppArmor' do
-#     code <<-END
-#         service apparmor stop
-#         update-rc.d -f apparmor remove
-#     END
-# end
-# dpkg_package "apparmor" do action :remove end
-# dpkg_package "apparmor-utils" do action :remove end
-
-execute 'Stop and remove sendmail' do
-    command 'service sendmail stop; update-rc.d -f sendmail remove'
-    action :nothing
-end
