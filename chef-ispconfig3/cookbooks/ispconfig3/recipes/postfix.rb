1b3306f8aab80455e48cae9c999d4d868a306e7b
Daniel Fanica <contact@danielfanica.com>
2015-12-31 00:09:37 +0000

    mysql secure install

--- chef-ispconfig3/cookbooks/ispconfig3/recipes/postfix.rb
+++ chef-ispconfig3/cookbooks/ispconfig3/recipes/postfix.rb
@@ -37,5 +37,21 @@ end
 # Open the TLS/SSL and submission ports in Postfix
 template ::File.join(node['ispconfig3']['mariadb_mysql_path'], 'mysqld.cnf') do
     source 'postfix/mysqld.cnf.erb'
+end
+
+package :mysql_secure_installation do
+    description 'Secure MySQL installation'
+    requires :mysql_core
+    apt 'expect'
+    local_file = 'mysql_secure_installation_no_ask'
+    remote_file = '/usr/local/mysql/scripts/mysql_secure_installation_no_ask'
+    transfer local_file, remote_file do
+        pre :install, 'mkdir -p /usr/local/mysql/scripts'
+        post :install, "chmod +x #{remote_file}"
+        post :install, remote_file
+    end
+    verify do
+        has_file remote_file
+    end
     notifies :restart, 'service[mysql]'
 end
