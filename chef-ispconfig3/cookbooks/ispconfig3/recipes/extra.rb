e6e048da257e1953e57c80c7ad68c9c003b68d26
Daniel Fanica <contact@danielfanica.com>
2015-12-31 10:04:12 +0000

    phpmyadmin

---
+++ chef-ispconfig3/cookbooks/ispconfig3/recipes/extra.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook Name:: ispconfig3
+# Recipe:: extra
+#
+# Copyright 2015, Daniel Fanica
+#
+# All rights reserved
+#
+
+# Install packages
+%w{
+    apache2-doc
+    memcached
+}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
