473198a4bb64ce7066096d954c2929a92f3b9c03
Daniel Fanica <contact@danielfanica.com>
2015-12-31 21:10:27 +0000

    enable ruby files on webserver

--- chef-ispconfig3/cookbooks/ispconfig3/recipes/apache_config.rb
+++ chef-ispconfig3/cookbooks/ispconfig3/recipes/apache_config.rb
@@ -30,3 +30,15 @@ template '/etc/apache2/mods-available/suphp.conf' do
     source 'suphp.conf.erb'
     notifies :restart, 'service[apache2]'
 end
+
+# to host Ruby files with the extension .rb on the web sites created through ISPConfig,
+# we must comment out the line `application/x-ruby rb` in /etc/mime.types
+ruby_block "enable ruby files on webserver" do
+  block do
+    rc = Chef::Util::FileEdit.new("/etc/mime.types")
+    rc.search_file_replace_line(
+      /^application/x-ruby /,
+      "# application/x-ruby rb"
+    )
+    rc.write_file
+  end
