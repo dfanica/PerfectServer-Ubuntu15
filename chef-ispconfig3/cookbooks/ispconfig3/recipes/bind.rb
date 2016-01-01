f6c2e592e1dd1f46176b20a18c581cb8f6ccc842
Daniel Fanica <contact@danielfanica.com>
2016-01-01 18:29:59 +0000

    bind

--- chef-ispconfig3/cookbooks/ispconfig3/recipes/bind.rb
+++ chef-ispconfig3/cookbooks/ispconfig3/recipes/bind.rb
@@ -7,12 +7,36 @@
 # All rights reserved
 #
 
-# Install packages
+# Install BIND DNS Server
+# Install Vlogger, Webalizer, And AWstats
+# Install Jailkit
 %w{
     bind9
     dnsutils
+    vlogger
+    webalizer
+    awstats
+    geoip-database
+    libclass-dbi-mysql-perl
+    build-essential
+    autoconf
+    automake1.9
+    libtool
+    flex
+    bison
+    debhelper
+    binutils-gold
 }.each do |pkg|
   package pkg do
     action :install
   end
+end
+
+file '/etc/cron.d/awstats' do content '' end
+
+# Jailkit is needed only if you want to chroot SSH users
+# http://olivier.sessink.nl/jailkit/jailkit-2.17.tar.gz
+tar_package 'http://olivier.sessink.nl/jailkit/jailkit-2.17.tar.gz' do
+    prefix '/tmp'
+    creates '/tmp/jailkit'
 end
