91c569402a88a3cc1b5fcd3eb3ea402dc7993084
Daniel Fanica <contact@danielfanica.com>
2016-01-03 12:53:07 +0000

    fix list_lists check

--- chef-ispconfig3/cookbooks/ispconfig3/recipes/mailman_new_list.rb
+++ chef-ispconfig3/cookbooks/ispconfig3/recipes/mailman_new_list.rb
@@ -1,7 +1,6 @@
 unless shell_out('list_lists').stdout.downcase.include?(node['mailman']['list_name'])
     # unless list_name_exists?
-        execute "newlist #{node['mailman']['list_name']}" do
-            command "newlist -l en -q mailman #{node['mailman']['email']} #{node['mailman']['password']}"
-        end
+    execute "newlist #{node['mailman']['list_name']}" do
+        command "newlist -l en -q mailman #{node['mailman']['email']} #{node['mailman']['password']}"
     end
 end
