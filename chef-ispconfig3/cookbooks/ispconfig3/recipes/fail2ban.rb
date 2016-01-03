dd3cbe35743f8616102832bff511b0e859c0e496
Daniel Fanica <contact@danielfanica.com>
2016-01-03 22:49:23 +0000

    fail2ban

---
+++ chef-ispconfig3/cookbooks/ispconfig3/recipes/fail2ban.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: ispconfig3
+# Recipe:: fail2ban
+#
+# Copyright 2015, Daniel Fanica
+#
+# All rights reserved
+#
+
+package 'fail2ban' do
+    action :install
+end
+
+template '/etc/fail2ban/jail.local' do
+    source 'jail.local.erb'
+end
+
+template '/etc/fail2ban/filter.d/pureftpd.conf' do
+    source 'pureftpd.conf.erb'
+end
+
+template '/etc/fail2ban/filter.d/dovecot-pop3imap.conf' do
+    source 'dovecot-pop3imap.conf.erb'
+end
+
+execute 'echo "ignoreregex =" >> /etc/fail2ban/filter.d/postfix-sasl.conf' do
+    notifies :restart, 'service[fail2ban]'
+end
