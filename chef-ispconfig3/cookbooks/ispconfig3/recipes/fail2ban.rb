8c0ed17982d1a626036f680f30a1eea3245e808e
Daniel Fanica <contact@danielfanica.com>
2016-01-04 09:43:05 +0000

    fail2ban config

--- chef-ispconfig3/cookbooks/ispconfig3/recipes/fail2ban.rb
+++ chef-ispconfig3/cookbooks/ispconfig3/recipes/fail2ban.rb
@@ -11,8 +11,16 @@ package 'fail2ban' do
     action :install
 end
 
+service "fail2ban" do
+  supports [ :status => true, :restart => true ]
+  action [ :enable, :start ]
+end
+
 template '/etc/fail2ban/jail.local' do
     source 'jail.local.erb'
+    owner 'root'
+    group 'root'
+    mode 0400
 end
 
 template '/etc/fail2ban/filter.d/pureftpd.conf' do
@@ -23,8 +31,11 @@ template '/etc/fail2ban/filter.d/dovecot-pop3imap.conf' do
     source 'dovecot-pop3imap.conf.erb'
 end
 
-execute 'echo "ignoreregex =" >> /etc/fail2ban/filter.d/postfix-sasl.conf'
-service "fail2ban" do
-    action :restart
-    ignore_failure true
+execute 'echo "ignoreregex =" >> /etc/fail2ban/filter.d/postfix-sasl.conf' do
+    notifies :restart, "service[fail2ban]"
 end
+
+# service "fail2ban" do
+#     action :restart
+#     ignore_failure true
+# end
