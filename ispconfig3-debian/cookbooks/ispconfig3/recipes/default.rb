62b4904353a79fd5403efced547a4b0f74cc2010
Daniel Fanica <contact@danielfanica.com>
2016-01-05 22:08:54 +0000

    debian8 update

--- ispconfig3-debian/cookbooks/ispconfig3/recipes/default.rb
+++ ispconfig3-debian/cookbooks/ispconfig3/recipes/default.rb
@@ -7,16 +7,6 @@
 # All rights reserved
 #
 
-# Edit /etc/apt/sources.list. Make sure that the universe and multiverse repositories are enabled.
-template '/etc/apt/sources.list' do
-    source 'sources.list.erb'
-end
-
-# update the system
-# include_recipe "apt"
-execute 'apt-get update'
-include_recipe 'hostupgrade::upgrade'
-
 # download and decompress the files needed for the installation
 bash 'ispconfig_setup-master' do
     code <<-EOH
