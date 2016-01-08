7d466486837c028f875f772565ad8f96b3468c3d
Daniel Fanica <contact@danielfanica.com>
2016-01-08 00:01:11 +0000

    depends list

--- ispconfig3-ubuntu15.04/cookbooks/ispconfig3/metadata.rb
+++ ispconfig3-ubuntu15.04/cookbooks/ispconfig3/metadata.rb
@@ -25,3 +25,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends          'apt'
+depends          'hostupgrade'
+depends          'apparmor'
+depends          'sendmail'
