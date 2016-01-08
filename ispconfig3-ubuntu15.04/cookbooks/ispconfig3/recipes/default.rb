6f0c7117f63cdad0046ab27a283eb21422f939e9
Daniel Fanica <contact@danielfanica.com>
2016-01-08 01:22:04 +0000

    Install Amavisd-new, SpamAssassin, And Clamav

--- ispconfig3-ubuntu15.04/cookbooks/ispconfig3/recipes/default.rb
+++ ispconfig3-ubuntu15.04/cookbooks/ispconfig3/recipes/default.rb
@@ -171,3 +171,30 @@ include_recipe 'zip'
         action :install
     end
 end
+
+# The ISPConfig 3 setup uses amavisd which loads the SpamAssassin filter library internally...
+# so we can stop SpamAssassin to free up some RAM
+service 'spamassassin' do action [ :stop, :disable ] end
+
+# Edit the clamd configuration file
+# Set AllowSupplementaryGroups to TRUE
+ruby_block "edit etc hosts" do
+    block do
+        rc = Chef::Util::FileEdit.new("/etc/clamav/clamd.conf")
+        rc.search_file_replace_line(
+            /^AllowSupplementaryGroups /,
+            "AllowSupplementaryGroups true"
+        )
+        rc.write_file
+    end
+end
+
+# stop if already running (in case server was restarted)
+# this will prevent `STDOUT: ERROR: /var/log/clamav/freshclam.log is locked by another process`
+service 'clamav-freshclam' do
+    action :stop
+end
+
+# start clamav
+execute 'freshclam'
+service 'clamav-daemon' do action :start end
