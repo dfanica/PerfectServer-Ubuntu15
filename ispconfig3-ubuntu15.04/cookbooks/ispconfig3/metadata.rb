dadd36e8de41f0785f1516d5d055185007d19b12
Daniel Fanica <contact@danielfanica.com>
2016-01-08 01:05:27 +0000

    magic_shell

--- ispconfig3-ubuntu15.04/cookbooks/ispconfig3/metadata.rb
+++ ispconfig3-ubuntu15.04/cookbooks/ispconfig3/metadata.rb
@@ -25,6 +25,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends          'apt'
+depends          'magic_shell'
 depends          'hostupgrade'
 depends          'apparmor'
 depends          'sendmail'
